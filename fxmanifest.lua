bf4ae4af6539a484ea0d29ab5b4d6ed0057e3f45
ESX Developer Portugal <88593228+zcmg@users.noreply.github.com>
2024-08-29 18:34:04 +0100

    Ainda em desenvolvimento

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'zcmg#5307'
-description 'v1.0'
+version '1.0'
 
 server_scripts {
 	'config.lua',
--- server.lua
+++ server.lua
@@ -1,22 +1,24 @@
 RegisterCommand('teste',function()
-    TriggerEvent('zcmg_debug:Send2Discord')
+    TriggerEvent('zcmg_debug:Send2Discord', GetCurrentResourceName(), 'teste99')
 end)
 
 RegisterServerEvent('zcmg_debug:Send2Discord')
 AddEventHandler('zcmg_debug:Send2Discord', function(resourcename, text)
-    local trigger
+    -- local trigger
 
-    AddEventHandler("gameEventTriggered", function(event, data)
-        trigger = event
-    end)
+    -- AddEventHandler("gameEventTriggered", function(event, data)
+    --     trigger = event
+    --     print(event)
 
-    -- PerformHttpRequest(Config.Webhook,)
+    -- end)
+
+   print('Logs de debug enviada para o discord')
 
     PerformHttpRequest(Config.Webhook, function(err, text, headers) end, "POST",
-		json.encode({ embeds = { { author = { name = " zcmg_debug", url = "https://fenixhub.dev/",
+		json.encode({ embeds = { { author = { name = resourcename, url = "https://fenixhub.dev/",
 			icon_url = "https://cdn.discordapp.com/attachments/636955559626670080/795310131704627231/logo-fen.png" },
-			title = "Anticheat Started...",
-			description = " FenixAC Started successful  \n  \nVersion: 1.0\n  \n Fenix_DevHub - " .. os.date(" %x  %X  %p"), color = 16711680, } } }),
+			title = "Debug Resource: "..resourcename.."Version",
+			description = " FenixAC Started successful  \n  \n"..GetResourceMetadata(GetCurrentResourceName(), 'version').."\n  \n Fenix_DevHub "" - " .. os.date(" %x  %X  %p"), color = 16711680, } } }),
 		{ ["Content-Type"] = "application/json" })
 
 end)
